572cfe72865ab1d29d9b7f2e044eaebfecfa9f73
Willy Go <willyg@users.noreply.github.com>
2020-09-01 10:44:39 +1000

    Force install

--- .install.sh
+++ .install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # TERM
-apt install xorg nodm rxvt-unicode-256color i3 fonts-inconsolata
+apt install xorg nodm rxvt-unicode-256color i3 fonts-inconsolata -y
 
 # Tools
-apt install vim tmux curl net-tools
+apt install vim tmux curl net-tools -y
 
 # For Virtualbox guest install
-apt install linux-headers-amd64 linux-headers-$(uname -r) make
+apt install linux-headers-amd64 linux-headers-$(uname -r) make -y

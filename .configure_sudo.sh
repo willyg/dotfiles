7a1e7fcecabd877f625bd7ac2f3298518054284d
Willy Go <willyg@users.noreply.github.com>
2020-09-01 12:32:15 +1000

    Reduce grub timeout

--- .configure_sudo.sh
+++ .configure_sudo.sh
@@ -2,11 +2,19 @@
 
 USERNAME=$1
 
-mkdir -p /etc/systemd/system/getty@tty1.service.d/
+if [ ! -d /etc/systemd/system/getty@tty1.service.d/ ]; then
+    echo "Set tty1 to autologin"
+    mkdir -p /etc/systemd/system/getty@tty1.service.d/
 
-cat <<EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
+    cat <<EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
 Type=simple
 ExecStart=
 ExecStart=-/usr/sbin/agetty --autologin $USERNAME --noclear %I $TERM
 EOF
+fi
+
+if ! grep -q timeout=1 /boot/grub/grub.cfg; then
+    echo 'Setting GRUB timeout to 1 second'
+    sed -i 's/timeout=5/timeout=1/g' /boot/grub/grub.cfg
+fi

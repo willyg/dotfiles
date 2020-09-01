1ff15f5ebc489e5e3d2eb59b178963bf7353ba12
Willy Go <willyg@users.noreply.github.com>
2020-09-01 11:51:26 +1000

    Update configure script

--- .configure.sh
+++ .configure.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-cat <<EOF >> .profile
+echo 'Start X on start'
+grep -q startx ~/.profile || cat <<EOF >> .profile
 
 if [[ -z "\$DISPLAY" ]] && [[ \$(tty) = /dev/tty1 ]]; then
     . startx
     logout
 fi
 EOF
+
+echo 'Generate i3 config'
+i3-config-wizard -m win
+grep -q new_window ~/.config/i3/config || echo 'new_window 1pixel' >> ~/.config/i3/config
+
+echo 'Set $TERM for ssh'
+grep -q ssh ~/.bashrc || echo "alias ssh='TERM=xterm-256color ssh'" >> ~/.bashrc
+
+echo 'Install vim-plug'
+if [ ! -d "~/.vim"]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+vim +'PlugInstall --sync' +qa
